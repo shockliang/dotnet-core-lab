3ae5dcb3573e17d5bf48f262ded031b23f706843
shocklaing <shockwater@gmail.com>
2017-09-27 13:59:58 +0800

    Initial commit

---
+++ console-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace console_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
